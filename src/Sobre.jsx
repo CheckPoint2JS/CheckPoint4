eb357fbb4459176a99766b5d27bc1a1a02c81f59
Guilherme <guicomercial328@gmail.com>
2023-09-06 16:42:05 -0300

    Estruturando a categoria Sobre

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -1,9 +1,17 @@
 import {} from 'react'
-import { NavLink } from 'react-router-dom'
+import Imagem from './assets/imagemSobre.jpg'
 
 function Sobre(){
     return(
-        <></>
+        <>
+        
+            <div>
+                <img src={Imagem} alt="Imagem-Sobre"/>
+
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione accusantium rem fuga nobis possimus optio labore sunt eius hic autem voluptates fugiat molestiae earum officia modi rerum commodi, qui omnis? Ut officiis vero, hic dolore sint eius dolor magni at dignissimos nisi dolores consequuntur maxime iste similique itaque fugit non, modi tenetur veritatis ratione esse inventore ipsum! Qui provident soluta totam id tempore nobis cum, reprehenderit, necessitatibus libero quo numquam nemo. Culpa voluptas dolorem deleniti alias! Id sint, culpa reiciendis fugiat iure explicabo ad quae eius soluta reprehenderit asperiores, sunt consectetur tempore esse autem sed tempora cumque fuga itaque consequuntur! Sit odit similique possimus alias autem eaque cum provident, necessitatibus expedita adipisci ab, cumque non dolore totam atque esse debitis quasi enim nulla rem voluptates minus a impedit ullam! Obcaecati unde, repellat quod optio itaque, commodi ratione incidunt laboriosam laudantium exercitationem minima facilis quos nostrum? Nesciunt nihil, debitis magnam voluptate dolore reprehenderit! At unde minus eius, rem delectus non culpa neque ratione officia recusandae magni? Cum quasi accusamus praesentium asperiores. Sed quas soluta, quisquam aut nemo aspernatur rem eligendi quos laudantium, voluptas voluptatibus natus tempore inventore repudiandae dolorum cum voluptates fuga odit nulla placeat praesentium expedita ut reiciendis. Hic, odit.</p>
+            </div>
+
+        </>
     )
 }
 

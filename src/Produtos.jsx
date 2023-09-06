22b9d1ed72dcfb2281c790d4dcd198da41b3bf63
Guilherme <guicomercial328@gmail.com>
2023-09-06 16:31:59 -0300

    Estruturando a pagina de Produtos

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -1,9 +1,34 @@
 import {} from 'react'
-import { NavLink } from 'react-router-dom'
+import './App.css'
+import Img1 from './assets/imagemLambo.jpg'
+import Img2 from './assets/imagemGTR.jpg'
+import Img3 from './assets/imagemFerrari.jpg'
+import Img4 from './assets/imagemAudi.jpg'
+
+
 
 function Produtos(){
     return(
-        <></>
+        <>
+        <h1>Modelos refências da categoria</h1>
+        <div>
+            <h2>Modelo 1</h2>
+           <img src={Img1} />
+        </div>
+        <div>
+            <h2>Modelo 2</h2>
+           <img src={Img2} />
+        </div>
+        <div>
+            <h2>Modelo 3</h2>
+           <img src={Img3} />
+        </div>
+        <div>
+            <h2>Modelo 4</h2>
+           <img src={Img4} />
+        </div>
+        
+        </>
     )
 }
 

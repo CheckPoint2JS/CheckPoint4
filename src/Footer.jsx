853b29bb31cb842988564deab55f8d90060d3cb4
Guilherme <guicomercial328@gmail.com>
2023-09-06 16:52:29 -0300

    Finalizado a estrutura do Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,13 @@ import { NavLink } from 'react-router-dom'
 
 function Footer(){
     return(
-        <></>
+        <>
+        
+        <div>
+            <h4>Entre em contato: 99999-9999 ou fulano@fiap.com</h4>
+        </div>
+       
+        </>
     )
 }
 

f08bef4ccf1b0c9daf795eba0b5f38516ea2fcc4
Guilherme <guicomercial328@gmail.com>
2023-09-06 15:02:22 -0300

    Criandos as rotas e navegação com os componentes criados ao App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import {  } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Nav from './Nav'
+import Home from './Home'
+import Produtos from './Produtos'
+import Sobre from './Sobre'
+import Footer from './Footer'
 
 function App() {
   
 
   return (
     <>
-
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Nav/>}/>
+        <Route path="/Home" element={<Home/>}/>
+        <Route path="/Produtos" element={<Produtos/>}/>
+        <Route path="/Sobre" element={<Sobre/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
     </>
   )
 }
